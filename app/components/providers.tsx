12c03c7c0643d5c17a4793d4c3df6baf6d9f6350
Alex Greco <alex.k.greco.4@gmail.com>
2024-12-12 19:55:25 -0700

    remove Prisma

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
 
 type ThemeProviderProps = Parameters<typeof ThemeProvider>[0];
 
 export function AppProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <ThemeProvider {...props} enableSystem>
-      <SessionProvider>{children}</SessionProvider>
+      {children}
     </ThemeProvider>
   );
 }
