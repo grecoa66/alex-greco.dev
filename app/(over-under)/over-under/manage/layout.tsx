b3bb6f77c72de211815af26d0c43f96f511156b1
Alex Greco <alex.k.greco.4@gmail.com>
2024-12-12 19:46:36 -0700

    Remove under over from the project.

--- app/(over-under)/over-under/manage/layout.tsx
+++
@@ -1,5 +0,0 @@
-const ManageLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="p-4">{children}</div>;
-};
-
-export default ManageLayout;
--- app/types/teams.ts
+++
@@ -1,6 +0,0 @@
-export type Team = {
-  [key: string]: {
-    team_name: string;
-    name_abbrev: string;
-  };
-};
