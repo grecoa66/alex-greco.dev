0a7288d1d5a5570b33c47869ef51cae688b56433
Alex Greco <alex.k.greco.4@gmail.com>
2025-01-10 07:42:53 -0700

    Figured out the animations and position of moons relative to the planets.

--- app/(portfolio)/css-playground/solar-system/page.tsx
+++ app/(portfolio)/css-playground/solar-system/page.tsx
@@ -4,10 +4,15 @@ const SolarSystem = () => {
     <div className="flex h-full w-full flex-row items-center justify-center overflow-hidden bg-white p-4 dark:bg-black">
       <div className="container h-[80%] w-[80%]">
         <div className="sun" />
+
         <div className="earth">
           <div className="moon" />
         </div>
-        <div className="mars" />
+
+        <div className="mars">
+          <div className="phobos" />
+          <div className="deimos" />
+        </div>
       </div>
     </div>
   );
